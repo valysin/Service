f31cb3470ab8a90c2abe59a6671adb69c1eed7cb
valysin <raguel_he@foxmail.com>
2019-10-16 00:02:17 +0800

    重构代码 添加dao层和service层

--- config/config.py
+++ config/config.py
@@ -18,21 +18,33 @@ REDIS = {
 LOCALHOST = '10.141.221.85'
 
 REPO_PATH = '/home/fdse/user/issueTracker/repo'
-
+LOCAL_ADDR_PREFIX = 'gitlab'
+REPO_ROOT_PATH_PATTERN = ''
 
 GITHUB_TOKEN = '1b87888beee96384cd45087f26394e90abdda5f4'
+GITLAB_TOKEN = ''
+GIT_API_URL = ''
+GIT_REMOTE_PREFIX = ''
 
-
+DOWNLOAD_ACCOUNT = {
+    'user':'',
+    'password':''
+}
 KAFKA_LOG_PATH = '/home/fdse/pythonApp/restfulAPI/log'
 
 KAFKA_HOST = {
     'host-1':'10.141.221.85:9092'
 }
 
+KAFKA_GROUP_ID = 'test-consumer-group'
+
 KAFKA_TOPIC = {
-    'topic-1':'RepoManager',
-    'topic-2':'ProjectManager',
-    'topic-3':'CompleteDownload'
+    'RepoManager':'RepoManager',
+    'ProjectManager':'ProjectManager',
+    'CompleteDownload':'CompleteDownload',
+    'Scan':'Scan',
+    'UpdateCommit':'UpdateCommit',
+    'DuplicateRepo':'DuplicateRepo'
 }
 
 REPO_PATH_TYPE = {
---
+++ dao/RepositoryDao.py
@@ -0,0 +1,30 @@
+from config import config
+from db.model import RepositoryModel
+
+
+class RepositoryDao:
+
+    def __init__(self, repository):
+        self.repository = repository
+
+
+    def insert(self):
+        from libs.tool import db_connect
+        DB = config.ISSUE_TRACKER_MYSQL_DB
+        session = db_connect(DB)
+        session.add(self.repository)
+        session.close()
+
+    def get_uuid_by_local_addr(self):
+        from libs.tool import db_connect
+        DB = config.ISSUE_TRACKER_MYSQL_DB
+        session = db_connect(DB)
+        ret = session.query(RepositoryModel.uuid).filter(RepositoryModel.local_addr == self.repository.local_addr).first()
+        session.close()
+        return ret
+
+    def delete(self):
+        from libs.tool import db_connect
+        DB = config.ISSUE_TRACKER_MYSQL_DB
+        session = db_connect(DB)
+        session.delete(self.repository)
